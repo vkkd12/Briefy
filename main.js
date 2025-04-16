77ee0974312b17de174200da4f20e5d5195d2d7e
Vividh Kanaujia <73335406+vkkd12@users.noreply.github.com>
2025-04-16 14:27:53 +0530

    Update main.js

--- main.js
+++ main.js
@@ -19,19 +19,26 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-app.post("/get", async (req, res) => {
-    const { email, mail } = req.body;
-    // decrpyt and validate token
+app.post("/store", async (req, res) => {
+    const { uid, mail } = req.body;
     const { subject, body } = mail;
     const summary = await summarise(body);
     const arr = summary[0].summary_text
     const category = get_category(subject + body);
-    const new_object = new DB_summary({ summary: arr, category: category, email: email });
+    const new_object = new DB_summary({ summary: arr, category: category, uid: uid });
     await new_object.save();
-    res.send(new_object._id);
+    res.status(200);
 });
 
-
+app.post("/get_summary", async (req, res) => {
+    const { uid } = req.body;
+    const mail = await DB_summary.find({ uid: uid });
+    if (mail) {
+        res.status(200).json(mail);
+    } else {
+        res.status(400);
+    }
+});
 
 // Connect to MongoDB
 const backendServer = app.listen(port, () => {
